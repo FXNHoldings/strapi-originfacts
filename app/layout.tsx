40ff91190756e280e60d5783b99214d8f63407e7
Vampy <kryptok112@gmail.com>
2026-09-05 18:11:10 +0800

    Add Google Tag Manager loader to the shared root head

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en" className={`${inter.variable} ${urbanist.variable} ${jakarta.variable} ${figtree.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T7MWHNST');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           {...({ nowprocket: '', 'nitro-exclude': '' } as Record<string, string>)}
           type="text/javascript"
